67ebd8040cfda54610836f86588285b726d2ff27
maya <andreas.mayr@cable.vol.at>
2018-04-21 16:32:21 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    
-    socket.emit('createMessage', {
-        from: 'test@example.com',
-        text: 'This is a test'
-    });
 });
 
 socket.on('disconnect', function() {
