e2099b4e14c2e92d3fad5717c3f1e117a3c3dd15
maya <andreas.mayr@cable.vol.at>
2018-04-21 21:02:51 +0200

    add geo location

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -12,5 +12,20 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text });
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message object', () => {
+
+        var from = 'andreas';
+        var lat = 9;
+        var long = 47;
+
+        var message = generateLocationMessage(from, lat, long);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message.from).toBe(from);
+        expect(message.url).toBe(`https://www.google.com/maps?q=${lat},${long}`,)
     });
 });
